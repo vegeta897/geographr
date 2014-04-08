3fc5fb2c2f3b5c9718e264279f247d1d58306e3b
vegeta897 <vegeta897@gmail.com>
2014-04-08 16:21:53 -0400

    Random noise lines drawn in forage activity to increase difficulty, changed cursor to crosshair

--- js/service-actCanvas.js
+++ js/service-actCanvas.js
@@ -11,11 +11,36 @@ angular.module('Geographr.actCanvas', [])
         eventLowCanvas.onselectstart = function() { return false; };
         eventMainCanvas.onselectstart = function() { return false; };
         eventHighCanvas.onselectstart = function() { return false; };
+        var mouse = [-1,-1];
+        eventHighCanvas.style.cursor = 'crosshair';
+        jQuery(eventHighCanvas).on('mousemove',function(e) {
+            return; // Not using this for now
+            var offset = jQuery(eventHighCanvas).offset(); // Get pixel location
+            var x = e.pageX - offset.left < 0 ? 0 : Math.floor((e.pageX - offset.left));
+            var y = e.pageY - offset.top < 0 ? 0 : Math.floor((e.pageY - offset.top));
+            if(mouse[0] != x && mouse[1] != y) {
+                mouse = [x,y];
+            }
+        });
+        var bgColor = '24272b';
         var flip = function() { return Math.random() > 0.5; }; // Flip a coin
         var clear = function() {
-            canvasUtility.fillCanvas(eventLowContext,'24272b');
+            canvasUtility.fillCanvas(eventLowContext,bgColor);
             eventMainContext.clearRect(0,0,300,300);
             eventHighContext.clearRect(0,0,300,300);
+        };
+        var drawNoiseLines = function(count,intensity) {
+            for(var i = 0; i < count; i++) {
+                eventLowContext.beginPath();
+                switch(Math.floor(Math.random()*2)) {
+                    case 0: eventLowContext.moveTo(0,Math.floor(Math.random()*300));
+                        eventLowContext.lineTo(300,Math.floor(Math.random()*300)); break;
+                    case 1: eventLowContext.moveTo(Math.floor(Math.random()*300),0);
+                        eventLowContext.lineTo(Math.floor(Math.random()*300),300); break;
+                }
+                var alpha = ',' + Math.floor(Math.random()*intensity)/100;
+                eventLowContext.strokeStyle = 'rgba(200,200,200' + alpha + ')'; eventLowContext.stroke();
+            }
         };
         return {
             fillCanvas: function(context,color) {
@@ -42,6 +67,7 @@ angular.module('Geographr.actCanvas', [])
                 clear(); // Clear event canvases
                 switch(type) {
                     case 'forage':
+                        Math.seedrandom(seed); drawNoiseLines(9,35); // Consistent noise
                         for(var p = 0; p < pool.length; p++) {
                             eventMainContext.beginPath();
                             eventMainContext.strokeStyle = '#' + pool[p].product.color;
